f4f0502ca060c7200ff998f734303d34d2bcea48
javascriptFan <wdeveloping9099@gmail.com>
2019-09-15 21:01:03 +0400

    clean project with cli

--- config/config.example.js
+++
@@ -1,6 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  db: 'mongodb://Star: @localhost:27017/db',
-  db_dev: 'mongodb://url:27017/db',
-};
--- routes/const.js
+++
@@ -1,5 +0,0 @@
-const CLIENT_ID = '361464668144-vav0qsh75f6noe33r2dqskp5eg1hahku.apps.googleusercontent.com';
-const CLIENT_SECRET = 'wOTxp_Sbn3rfHuWfyFQscxVc';
-
-module.exports.CLIENT_ID = CLIENT_ID;
-module.exports.CLIENT_SECRET = CLIENT_SECRET;
