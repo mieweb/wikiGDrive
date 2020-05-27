2a74d866fc8b6ff0ba290f3e0ce18d73e5afc55f
Grzegorz Godlewski <gg@gitgis.com>
2020-05-27 19:47:34 +0200

    Fix test

--- test/QuotaLimiterTest.js
+++ test/QuotaLimiterTest.js
@@ -1,12 +1,21 @@
 import { assert } from 'chai';
+import sinon from 'sinon';
 import {QuotaLimiter} from '../src/google/QuotaLimiter';
+
+var clock;
+
+before(function () { clock = sinon.useFakeTimers(); });
+after(function () { clock.restore(); });
 
 describe('QuotaLimiterTest', () => {
   it('test sortLimits', async () => {
     const limiter = new QuotaLimiter();
     limiter.addLimit(5, 2);
+    clock.tick(20 * 1000);
     limiter.addLimit(3, 1);
+    clock.tick(20 * 1000);
     limiter.addLimit(5, 3);
+    clock.tick(20 * 1000);
     limiter.addLimit(5, 1);
 
     assert.equal(limiter.limits[0].seconds, 1);
