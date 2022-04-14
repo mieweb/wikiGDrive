c98b1c8e087ec5a1eabcf914e3a366b8fc567b55
Grzegorz Godlewski <gg@gitgis.com>
2022-04-14 18:18:51 +0200

    Fix ts-node start

--- src/cli.sh
+++ src/cli.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-/usr/bin/env node --no-warnings --experimental-specifier-resolution=node --loader ts-node/esm src/main.ts "$@"
+
+FULL_PATH="$(readlink -f ${BASH_SOURCE[0]})"
+MAIN_DIR=$(dirname "$FULL_PATH")/..
+NODE_MODULES=$MAIN_DIR/node_modules
+
+#export NODE_PATH=$NODE_MODULES
+cd $MAIN_DIR
+
+/usr/bin/env node --no-warnings --experimental-specifier-resolution=node --loader $NODE_MODULES/ts-node/esm $MAIN_DIR/src/main.ts "$@"
