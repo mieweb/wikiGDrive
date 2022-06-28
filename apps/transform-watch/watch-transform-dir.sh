eabd861f2702ba7b2e27304ba3c6d8534804d3df
Grzegorz Godlewski <gg@gitgis.com>
2022-06-28 23:37:48 +0200

    Fix transform config

--- apps/transform-watch/watch-transform-dir.sh
+++ apps/transform-watch/watch-transform-dir.sh
@@ -10,6 +10,7 @@ fi
 render() {
     echo docker run -v "$VOLUME_DATA/$1_transform:/site/content" -v "$VOLUME_PREVIEW/$1:/site/public" hugo-render
     docker run \
+        --env BASE_URL=$DOMAIN/preview/$1/ \
         --mount type=bind,source="$VOLUME_DATA/$1_transform",target=/site/content \
         --mount type=bind,source="$VOLUME_PREVIEW/$1",target=/site/public \
         $RENDER_IMAGE
@@ -25,5 +26,6 @@ do
     if [ ".tree.json" = "$FILE" ]
     then
         render $1
+        chown www-data:www-data -R "/preview/$1"
     fi
 done
