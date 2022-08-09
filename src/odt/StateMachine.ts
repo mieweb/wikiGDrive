28fd01eb5746ec10341e2cb6b9ffa90d87bc36e9
Grzegorz Godlewski <gg@gitgis.com>
2022-08-09 13:50:47 +0200

    Fix spaces inside bold and italic

--- src/odt/StateMachine.ts
+++ src/odt/StateMachine.ts
@@ -355,6 +355,27 @@ export class StateMachine {
       }
     }
 
+    for (let position = 1; position < this.markdownChunks.length; position++) {
+      const chunk = this.markdownChunks.chunks[position];
+      if (chunk.isTag && ['/B', '/I'].indexOf(chunk.tag) > -1) {
+        const prevChunk = this.markdownChunks.chunks[position - 1];
+        if (prevChunk.isTag === false && prevChunk.mode === 'md') {
+          const text = prevChunk.text;
+          const removedTrailingSpaces = text.replace(/[\s]+$/, '');
+          const spaces = text.substring(removedTrailingSpaces.length);
+          if (spaces.length > 0) {
+            prevChunk.text = removedTrailingSpaces;
+            this.markdownChunks.chunks.splice(position + 1, 0, {
+              isTag: false,
+              mode: 'md',
+              text: spaces
+            });
+            position++;
+          }
+        }
+      }
+    }
+
     const matching = {
       '/B': 'B',
       '/I': 'I',
