9f27ef0f5c76ca4fcea4efe8ac8264492edab467
Grzegorz Godlewski <gg@gitgis.com>
2022-08-30 20:58:27 +0200

    Fix images

--- src/containers/transform/TaskLocalFileTransform.ts
+++ src/containers/transform/TaskLocalFileTransform.ts
@@ -135,7 +135,7 @@ export class TaskLocalFileTransform extends QueueTask {
     }
 
     const converter = new OdtToMarkdown(document, styles);
-    converter.setPicturesDir(this.realFileName.replace('.md', '.assets/'));
+    converter.setPicturesDir('../' + this.realFileName.replace('.md', '.assets/'));
     const markdown = await converter.convert();
     const frontMatter = generateDocumentFrontMatter(localFile, hierarchy, Array.from(converter.links));
     await this.destinationDirectory.writeFile(this.realFileName, frontMatter + markdown);
