575e70365829cfa5d072aec79363d1c05d946014
Grzegorz Godlewski <gg@gitgis.com>
2022-10-06 12:41:19 +0200

    Add network caching to vue

--- apps/ui/assets/services/AuthenticatedClient.mjs
+++ apps/ui/assets/services/AuthenticatedClient.mjs
@@ -8,9 +8,21 @@ export class AuthenticatedClient {
 
   constructor(app) {
     this.app = app;
+    this.lastGetReponses = {};
   }
 
   async fetchApi(url, params = {}) {
+    const now = +new Date();
+
+    const isGet = Object.keys(params).length === 0;
+    if (isGet && this.lastGetReponses[url]) {
+      if (now - this.lastGetReponses[url].ts < 1000) {
+        return this.lastGetReponses[url].response;
+      } else {
+        delete this.lastGetReponses[url];
+      }
+    }
+
     if (!params.headers) {
       params.headers = {};
     }
@@ -38,6 +50,18 @@ export class AuthenticatedClient {
         if ((response.headers.get('Content-type') || '').startsWith('text/plain')) {
           throw new Error(await response.text());
         }
+    }
+
+    if (isGet) {
+      const contentTYpe = (response.headers.get('Content-type') || '');
+      if (contentTYpe.startsWith('application/json') || contentTYpe.startsWith('application/vnd.google-apps.folder')) {
+        const json = await response.json();
+        response.json = async () => json;
+      }
+      this.lastGetReponses[url] = {
+        ts: now,
+        response
+      }
     }
 
     return response;
