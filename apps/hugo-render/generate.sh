6ab7fd2b96a15069f0bd497975288be2c2e70357
Grzegorz Godlewski <gg@gitgis.com>
2022-10-25 18:50:25 +0200

    Fix hugo render

--- apps/hugo-render/generate.sh
+++ apps/hugo-render/generate.sh
@@ -4,9 +4,15 @@ cd /site
 
 if [[ ! -z $THEME_ID ]]
 then
-  echo "Using theme $THEME_URL $THEME_SUBPATH"
-
-  git clone $THEME_URL themes/$THEME_ID
+  if [[ -d /themes/$THEME_ID ]]
+  then
+    [[ -d themes ]] || mkdir themes
+    echo "Linking theme $THEME_ID to /themes/$THEME_ID"
+    ln -sf /themes/$THEME_ID themes/$THEME_ID
+  else
+    echo "Using theme $THEME_URL $THEME_SUBPATH"
+    git clone $THEME_URL themes/$THEME_ID
+  fi
 
   if [[ ! -z "$THEME_SUBPATH" ]]
   then
@@ -23,4 +29,8 @@ fi
 
 hugo --config=/site/tmp_dir/config.toml --verbose
 
+CODE=$?
+
 rm -rf .hugo_build.lock
+
+exit $CODE
