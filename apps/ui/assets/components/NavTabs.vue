1f43d2ed8dfefed04a2d844953f3b7ce279721f8
Grzegorz Godlewski <gg@gitgis.com>
2022-11-20 16:40:07 +0100

    Add vue telemetry

--- apps/ui/assets/components/NavTabs.vue
+++ apps/ui/assets/components/NavTabs.vue
@@ -2,29 +2,29 @@
   <ul class="navbar-nav mr-auto">
     <li :class="{ 'active': activeTab === 'html' || activeTab === 'markdown' }" class="wgd-nav-item"
         v-if="isDocument(selectedFile) || isMarkdown(selectedFile) || isImage(selectedFile)">
-      <a @click.prevent.stop="setActiveTab('html')">
+      <a @click.prevent.stop="setActiveTab('html')" href="#html">
         <i class="fa-solid fa-eye"></i>
       </a>
     </li>
     <li :class="{ 'active': !activeTab }" class="wgd-nav-item"
         v-else-if="activeTab === 'git_commit'">
-      <a @click.prevent.stop="setActiveTab('')">
+      <a @click.prevent.stop="setActiveTab('')" href="#">
         <i class="fa-solid fa-eye"></i>
       </a>
     </li>
     <li :class="{ 'active': !activeTab || activeTab === 'html' || activeTab === 'markdown' }" class="wgd-nav-item"
         v-else-if="selectedFolder">
-      <a @click.prevent.stop="setActiveTab('')">
+      <a @click.prevent.stop="setActiveTab('')"  href="#">
         <i class="fa-solid fa-eye"></i>
       </a>
     </li>
     <li v-if="isGDocsPreview" :class="{ 'active': activeTab.startsWith('drive_') }" class="wgd-nav-item">
-      <a @click.prevent.stop="setActiveTab('drive_tools')">
+      <a @click.prevent.stop="setActiveTab('drive_tools')" href="#drive_tools">
         <i class="fa-brands fa-google-drive"></i>
       </a>
     </li>
     <li :class="{ 'active': activeTab.startsWith('git_') }" class="wgd-nav-item">
-      <a class="position-relative" @click.prevent.stop="setActiveTab('git_commit')">
+      <a class="position-relative" @click.prevent.stop="setActiveTab('git_commit')" href="#git_commit">
         <i class="fa-brands fa-git-square"></i>
         <span class="position-absolute top-0 changes-badge translate-middle badge rounded-pill bg-danger" v-if="gitStats.unstaged > 0">
            <span class="badge badge-primary" v-if="gitStats.unstaged">{{gitStats.unstaged}}</span>
@@ -32,7 +32,7 @@
       </a>
     </li>
     <li :class="{ 'active': ['performance', 'drive_logs', 'drive_config', 'sync'].includes(activeTab) }" class="wgd-nav-item">
-      <a class="position-relative" @click.prevent.stop="setActiveTab('sync')">
+      <a class="position-relative" @click.prevent.stop="setActiveTab('sync')" href="#sync">
         <i class="fa-solid fa-rotate" :class="{'fa-spin': syncing}"></i>
         <span class="position-absolute top-0 changes-badge translate-middle badge rounded-pill bg-danger" v-if="fileChanges.length > 0">
            <span class="badge badge-primary">{{fileChanges.length}}</span>
--- apps/ui/assets/telemetry.ts
+++ apps/ui/assets/telemetry.ts
@@ -2,16 +2,19 @@ import {
   BatchSpanProcessor, ReadableSpan,
   SimpleSpanProcessor, SpanExporter
 } from '@opentelemetry/sdk-trace-base';
+import {
+  InstrumentationBase,
+  registerInstrumentations
+} from '@opentelemetry/instrumentation';
+import opentelemetry, {Span, SpanKind} from '@opentelemetry/api';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { ExportResult, ExportResultCode } from '@opentelemetry/core';
-import * as api from '@opentelemetry/api';
-import {FetchError} from '@opentelemetry/instrumentation-fetch/build/src/types';
+import { FetchError } from '@opentelemetry/instrumentation-fetch/build/src/types';
 
 const metaEl = document.querySelector('meta[name=ZIPKIN_URL]');
 const ZIPKIN_URL = metaEl ? metaEl.getAttribute('content') : undefined;
@@ -58,13 +61,77 @@ export class InMemorySpanExporter implements SpanExporter {
   }
 }
 
+let provider;
+
+export class VueComponentInstrumentation extends InstrumentationBase<unknown> {
+
+  constructor(private component) {
+    super('instrumentation-vue', '1.0', { enabled: false });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  protected init() {}
+
+  override enable() {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const instrumentation = this;
+
+    if (this.component.name && this.component.methods) {
+      for (const methodName in this.component.methods) {
+        if ('function' !== typeof this.component.methods[methodName]) {
+          continue;
+        }
+
+        const origMethod = this.component.methods[methodName];
+        if (origMethod.telemetryDisable) {
+          continue;
+        }
+
+        this.component.methods[methodName] = async function (...args) {
+          const stackTrace = new Error().stack.split('\n').splice(2);
+          const tracer = opentelemetry.trace.getTracer(
+            provider.resource.attributes[SemanticResourceAttributes.SERVICE_NAME].toString(),
+            '1.0'
+          );
+
+          let spanName = `${instrumentation.component.name}.${methodName}`;
+          if (origMethod.telemetryParamCount) {
+            spanName += '(';
+            for (let i = 0; i < origMethod.telemetryParamCount; i++) {
+              if (i > 0) {
+                spanName += ', ';
+              }
+              spanName += args[0];
+            }
+            spanName += ')';
+          }
+
+          return tracer.startActiveSpan(spanName, { kind: SpanKind.INTERNAL }, async (span) => {
+            try {
+              return await origMethod.apply(this, args);
+            } catch (err) {
+              err.stack = [err.message].concat(stackTrace).join('\n');
+              span.recordException(err);
+              throw err;
+            } finally {
+              span.end();
+            }
+          });
+        };
+        this.component.methods[methodName].origMethod = origMethod;
+      }
+    }
+
+  }
+}
+
 export function addTelemetry(app) {
   if (ZIPKIN_URL) {
     const serviceName = import.meta.env.VITE_APP_ZIPKIN_SERVICE || 'wikigdrive';
 
     console.log('Initializing telemetry', serviceName, ZIPKIN_URL);
 
-    const provider = new WebTracerProvider({
+    provider = new WebTracerProvider({
       resource: new Resource({
         [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         [SemanticResourceAttributes.SERVICE_VERSION]: '1.0'
@@ -87,7 +154,7 @@ export function addTelemetry(app) {
       instrumentations: [
         new FetchInstrumentation({
           ignoreUrls: [],
-          applyCustomAttributesOnSpan(span: api.Span, request: Request | RequestInit, result: Response | FetchError) {
+          applyCustomAttributesOnSpan(span: Span, request: Request | RequestInit, result: Response | FetchError) {
             if (result['url'] && result['url'].indexOf('/api/') > -1) {
               const method = request.method || 'get';
               const url = new URL(result['url']);
@@ -100,5 +167,13 @@ export function addTelemetry(app) {
       ],
     });
 
+    const router = app.config.globalProperties.$router;
+    router.afterEach((to) => {
+      for (const match of to.matched) {
+        registerInstrumentations({
+          instrumentations: Object.values(match.components).map(component => new VueComponentInstrumentation(component))
+        });
+      }
+    });
   }
 }
