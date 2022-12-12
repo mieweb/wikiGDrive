15dd5c638d587fb56fedea7be52f94da6605613f
Grzegorz Godlewski <gg@gitgis.com>
2022-12-12 22:02:56 +0100

    Fix vite config

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -5,12 +5,25 @@ if (!process.env.BUILD_TIME) {
   process.env.BUILD_TIME = new Date().toISOString();
 }
 if (!process.env.VERSION) {
-  process.env.VERSION = process.env.GIT_SHA || 'dev';
+  process.env.VERSION = process.env.GIT_SHA || process.env.GITHUB_SHA || 'dev';
 }
+
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(
+        /GIT_SHA/g,
+        process.env.VERSION,
+      );
+    },
+  };
+};
 
 export default defineConfig({
   plugins: [
     vue(),
+    htmlPlugin()
   ],
   resolve: {
     alias: {
@@ -20,9 +33,10 @@ export default defineConfig({
   build: {
     sourcemap: true,
     manifest: true,
+    target: 'esnext'
   },
   define: {
-    'import.meta.env.GIT_SHA': JSON.stringify(process.env.GIT_SHA),
+    'import.meta.env.GIT_SHA': JSON.stringify(process.env.GIT_SHA || process.env.GITHUB_SHA || 'dev'),
     'import.meta.env.BUILD_TIME': JSON.stringify(process.env.BUILD_TIME),
     'import.meta.env.VERSION': JSON.stringify(process.env.VERSION),
     'import.meta.env.VITE_APP_ZIPKIN_SERVICE': JSON.stringify(process.env.ZIPKIN_SERVICE)
