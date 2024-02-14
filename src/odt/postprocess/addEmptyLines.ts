125fbfa17df668975a1f9d57ea34d55ab277976e
Grzegorz Godlewski <gg@gitgis.com>
2024-02-14 19:42:13 +0100

    Decouple postprocessing functions

---
+++ src/odt/postprocess/addEmptyLines.ts
@@ -0,0 +1,37 @@
+import {MarkdownChunks} from '../MarkdownChunks.js';
+
+export function addEmptyLines(markdownChunks: MarkdownChunks) {
+
+  for (let position = 0; position < markdownChunks.length; position++) {
+    const chunk = markdownChunks.chunks[position];
+
+    if (position + 1 < markdownChunks.chunks.length && chunk.isTag && ['/H1', '/H2', '/H3', '/H4', 'IMG/', 'SVG/'].indexOf(chunk.tag) > -1) {
+      const nextTag = markdownChunks.chunks[position + 1];
+
+      if (!(nextTag.isTag && nextTag.tag === 'BR/')) {
+        markdownChunks.chunks.splice(position + 1, 0, {
+          isTag: true,
+          mode: 'md',
+          tag: 'BR/',
+          payload: {},
+          comment: 'Next tag is not BR/'
+        });
+      }
+    }
+
+    if (position > 1 && chunk.isTag && ['H1', 'H2', 'H3', 'H4', 'IMG/', 'SVG/'].indexOf(chunk.tag) > -1) {
+      const prevTag = markdownChunks.chunks[position - 1];
+      if (!(prevTag.isTag && prevTag.tag === 'BR/')) {
+        markdownChunks.chunks.splice(position - 1, 0, {
+          isTag: false,
+          mode: 'md',
+          text: '\n',
+          // payload: {},
+          comment: 'Add empty line before: ' + chunk.tag
+        });
+        position++;
+      }
+    }
+  }
+
+}
