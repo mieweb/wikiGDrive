a54b2359a1606ead8f708636e945a686f57cfd1d
Grzegorz Godlewski <gg@gitgis.com>
2024-02-16 14:04:26 +0100

    Remove breaks around images

--- src/odt/postprocess/addEmptyLines.ts
+++ src/odt/postprocess/addEmptyLines.ts
@@ -5,7 +5,7 @@ export function addEmptyLines(markdownChunks: MarkdownChunks) {
   for (let position = 0; position < markdownChunks.length; position++) {
     const chunk = markdownChunks.chunks[position];
 
-    if (position + 1 < markdownChunks.chunks.length && chunk.isTag && ['/H1', '/H2', '/H3', '/H4', 'IMG/', 'SVG/', '/UL'].indexOf(chunk.tag) > -1) {
+    if (position + 1 < markdownChunks.chunks.length && chunk.isTag && ['/H1', '/H2', '/H3', '/H4', 'SVG/', '/UL'].indexOf(chunk.tag) > -1) {
       const nextTag = markdownChunks.chunks[position + 1];
 
       if (!(nextTag.isTag && nextTag.tag === 'BR/') && !(nextTag.isTag && nextTag.tag === '/TD')) {
@@ -19,10 +19,10 @@ export function addEmptyLines(markdownChunks: MarkdownChunks) {
       }
     }
 
-    if (position > 1 && chunk.isTag && ['H1', 'H2', 'H3', 'H4', 'IMG/', 'SVG/', 'UL'].indexOf(chunk.tag) > -1) {
+    if (position > 1 && chunk.isTag && ['H1', 'H2', 'H3', 'H4', 'SVG/', 'UL'].indexOf(chunk.tag) > -1) {
       const prevTag = markdownChunks.chunks[position - 1];
       if (!(prevTag.isTag && prevTag.tag === 'BR/') && !(prevTag.isTag && prevTag.tag === 'TD')) {
-        markdownChunks.chunks.splice(position - 1, 0, {
+        markdownChunks.chunks.splice(position, 0, {
           isTag: false,
           mode: 'md',
           text: '\n',
