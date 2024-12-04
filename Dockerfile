f75b35d015ad9391affd353a9c654f06e6686d43
Grzegorz Godlewski <gg@gitgis.com>
2024-12-04 13:37:58 +0100

    Fix bugs

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,28 @@ FROM node:22-bookworm-slim
 
 ARG BUILD_UI
 ARG GIT_SHA
+EXPOSE 3000
+VOLUME /data
+WORKDIR /usr/src/app
 
 RUN apt-get update
 RUN apt-get install -yq bash git-lfs openssh-client curl unzip socat
 RUN curl -fsSL https://deno.land/install.sh | DENO_INSTALL=/usr/local sh
-WORKDIR /usr/src/app
 
 COPY package.json package-lock.json ./
-RUN npm install
-RUN npm install --location=global ts-node
-
-RUN if [ -z "$BUILD_UI" ] ; then cd /usr/src/app/apps/ui && npm run build ; fi
+COPY deno.json deno.lock ./
+#RUN npm install
+#RUN npm install --location=global ts-node
 
 COPY . ./
-RUN npm link --location=user
 
-EXPOSE 3000
-VOLUME /data
+RUN deno install
+#RUN if [ -z "$BUILD_UI" ] ; then cd /usr/src/app/apps/ui && npm run build ; fi
+#RUN npm link --location=user
+RUN ln -sf /usr/src/app/src/wikigdrive.sh /usr/local/bin/wikigdrive
+RUN ln -sf /usr/src/app/src/wikigdrivectl.sh /usr/local/bin/wikigdrivectl
 
-RUN cd /usr/src/app/apps/ui && npm install && npm run build
+RUN npm install && npm run build --workspaces
 
 WORKDIR "/usr/src/app"
 
--- apps/ui/src/main.ts
+++ apps/ui/src/main.ts
@@ -1,10 +1,8 @@
 import {Tooltip} from 'bootstrap';
 import {createApp} from './app.ts';
 import {addTelemetry} from './telemetry.ts';
-import {Emitter, EventType} from 'mitt';
 
 const { app, router } = createApp();
-await router.isReady();
 addTelemetry(app);
 
 app.mixin({
@@ -27,48 +25,50 @@ if (!import.meta.env.SSR) {
   }
 }
 
-const vm= app.mount('#app', true);
-router.beforeEach(async (to, from, next) => {
-  if (to.meta.ssg) {
-    try {
-      const response = await fetch(window.location.protocol + '//' + window.location.host + to.path);
-      const html = await response.text();
+router.isReady().then(() => {
+  const vm = app.mount('#app', true);
+  router.beforeEach(async (to, from, next) => {
+    if (to.meta.ssg) {
+      try {
+        const response = await fetch(window.location.protocol + '//' + window.location.host + to.path);
+        const html = await response.text();
 
-      const parser = new DOMParser();
-      const r = parser.parseFromString(html, 'text/html');
-      const titleElem = r.querySelector('title');
-      if (titleElem) {
-        document.title = titleElem.innerText;
+        const parser = new DOMParser();
+        const r = parser.parseFromString(html, 'text/html');
+        const titleElem = r.querySelector('title');
+        if (titleElem) {
+          document.title = titleElem.innerText;
+        }
+        const mainContent = r.querySelector('.mainbar__content');
+        const elemContent = document.querySelector('.mainbar__content');
+        if (mainContent && elemContent) {
+          setTimeout(() => {
+            const emitter = (vm as any).emitter;
+            emitter.emit('html_lazy_content', mainContent.innerHTML);
+          }, 100);
+        }
+        next(true);
+        return;
+      } catch (err) {
+        console.error(err);
       }
-      const mainContent = r.querySelector('.mainbar__content');
-      const elemContent = document.querySelector('.mainbar__content');
-      if (mainContent && elemContent) {
-        setTimeout(() => {
-          const emitter = (vm as any).emitter;
-          emitter.emit('html_lazy_content', mainContent.innerHTML);
-        }, 100);
-      }
-      next(true);
-      return;
-    } catch (err) {
-      console.error(err);
     }
-  }
 
-  const toDriveId = Array.isArray(to.params?.driveId) ? to.params.driveId[0] : to.params.driveId;
-  const fromDriveId = Array.isArray(from.params?.driveId) ? from.params.driveId[0] : from.params.driveId;
-  if (toDriveId !== fromDriveId) {
-    await (vm as any).FileClientService.clearCache();
-    await (vm as any).changeDrive(toDriveId);
-  }
-  next();
-});
-router.afterEach(() => {
-  const elements = document.querySelectorAll('[data-bs-toggle=tooltip]');
-  elements.forEach(element => {
-    const tooltip = Tooltip.getInstance(element);
-    if (tooltip) {
-      tooltip.hide();
+    const toDriveId = Array.isArray(to.params?.driveId) ? to.params.driveId[0] : to.params.driveId;
+    const fromDriveId = Array.isArray(from.params?.driveId) ? from.params.driveId[0] : from.params.driveId;
+    if (toDriveId !== fromDriveId) {
+      await (vm as any).FileClientService.clearCache();
+      await (vm as any).changeDrive(toDriveId);
     }
+    next();
+  });
+  router.afterEach(() => {
+    const elements = document.querySelectorAll('[data-bs-toggle=tooltip]');
+    elements.forEach(element => {
+      const tooltip = Tooltip.getInstance(element);
+      if (tooltip) {
+        tooltip.hide();
+      }
+    });
   });
 });
