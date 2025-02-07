ba8923eb9d87ece97cc4deddda96f7dd47d67a15
Grzegorz Godlewski <gg@gitgis.com>
2025-02-07 20:51:27 +0100

    Fix crypto cookie decrypt

--- test/encryptTest.ts
+++ test/encryptTest.ts
@@ -1,9 +1,9 @@
 import test from './tester.ts';
 import {decrypt, encrypt} from '../src/google/GoogleAuthService.ts';
 
-test('test encryption', (t) => {
+test('test encryption', async (t) => {
   const dec = 'plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123';
-  const enc = encrypt(dec, 'sikretsikretsikretsikretsikret');
-  const dec2 = decrypt(enc, 'sikretsikretsikretsikretsikret');
+  const enc = await encrypt(dec, 'sikretsikretsikretsikretsikret');
+  const dec2 = await decrypt(enc, 'sikretsikretsikretsikretsikret');
   t.is(dec, dec2);
 });
