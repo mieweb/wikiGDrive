2e3060296d29cefb1832d3ae7657d8fa30b9b3c5
Grzegorz Godlewski <gg@gitgis.com>
2025-02-27 19:20:57 +0100

    Drop ava dep

--- test/encryptTest.ts
+++ test/encryptTest.ts
@@ -1,9 +1,10 @@
-import test from './tester.ts';
+import {assertStrictEquals} from 'asserts';
+
 import {decrypt, encrypt} from '../src/google/GoogleAuthService.ts';
 
-test('test encryption', async (t) => {
+Deno.test('test encryption', async () => {
   const dec = 'plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123plaintext123';
   const enc = await encrypt(dec, 'sikretsikretsikretsikretsikret');
   const dec2 = await decrypt(enc, 'sikretsikretsikretsikretsikret');
-  t.is(dec, dec2);
+  assertStrictEquals(dec, dec2);
 });
