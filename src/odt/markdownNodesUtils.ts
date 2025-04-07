a02ccf70944c2a8e7efd77cdc9e83025ca5f8655
Grzegorz Godlewski <gg@gitgis.com>
2025-04-07 19:38:59 +0200

    Fix romanize iteration bug

--- src/odt/markdownNodesUtils.ts
+++ src/odt/markdownNodesUtils.ts
@@ -87,13 +87,28 @@ interface ToTextContext {
   parentLevel?: number;
 }
 
-function romanize(num: number) {
-  const lookup: Record<string, number> = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+function romanize(num: number): string {
+  const lookup: [string, number][] = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
   let roman = '';
-  for (const i in lookup) {
-    while (num >= lookup[i]) {
-      roman += i;
-      num -= lookup[i];
+  for (const [symbol, value] of lookup) {
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
     }
   }
   return roman;
