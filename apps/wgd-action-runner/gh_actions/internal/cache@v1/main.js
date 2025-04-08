97747241b34a071a0bd56a5bf39606f9ad078573
Grzegorz Godlewski <gg@gitgis.com>
2025-04-08 16:14:46 +0200

    Add fallback for cache errors

--- apps/wgd-action-runner/gh_actions/internal/cache@v1/main.js
+++ apps/wgd-action-runner/gh_actions/internal/cache@v1/main.js
@@ -11,7 +11,21 @@ const targetDir = '/site/' + process.env.INPUT_PATH;
 
 console.log('Cache action restoring: ' + process.env.INPUT_PATH + ' ' + (fs.existsSync(sourceDir) ? 'restoring' : 'skipping'));
 
-if (fs.existsSync(sourceDir)) {
-  fs.mkdirSync(targetDir, { recursive: true });
-  fs.cpSync(sourceDir, targetDir, { recursive: true });
+try {
+  if (fs.existsSync(sourceDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+    fs.cpSync(sourceDir, targetDir, { recursive: true });
+  }
+} catch (err) {
+  console.error(err);
+  try {
+    console.info('Removing: ' + targetDir);
+    fs.rmSync(targetDir, { recursive: true, force: true });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) { /* empty */ }
+  try {
+    console.info('Removing cache');
+    fs.rmSync(sourceDir, { recursive: true, force: true });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) { /* empty */ }
 }
