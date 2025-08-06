b3426c0ae56031c46f49aaf3e34adadbacc7ddcf
Grzegorz Godlewski <gg@gitgis.com>
2025-08-06 22:09:47 +0200

    Fix tests

--- test/RelatedUrlTest.ts
+++ test/RelatedUrlTest.ts
@@ -9,6 +9,7 @@ Deno.test('test relateUrl', () => {
   assertStrictEquals(relateUrl('/etc/http', '/etc/log'), '../log');
   assertStrictEquals(relateUrl('content/functions/document-management/documents-and-forms/biometric-data-entry.md', 'content/functions/document-management/scanning-and-indexing/indexing-bubble-forms.md'), '../scanning-and-indexing/indexing-bubble-forms.md');
   assertStrictEquals(relateUrl('somefile.md', 'https://youtube.be/v/xxx'), 'https://youtube.be/v/xxx');
+  assertStrictEquals(relateUrl('somefile.md', 'https://youtube.be/v/xxx?que&r=y'), 'https://youtube.be/v/xxx?que&r=y');
 });
 
 Deno.test('test absolutizeUrl', () => {
@@ -20,4 +21,6 @@ Deno.test('test absolutizeUrl', () => {
   assertStrictEquals(absolutizeUrl('https://example.com/path/path2', '../..'), 'https://example.com/');
   assertStrictEquals(absolutizeUrl('https://example.com/path/path2', '../../path3'), 'https://example.com/path3');
   assertStrictEquals(absolutizeUrl('https://example.com/path/path2', '../../../path3'), 'https://example.com/path3');
+  assertStrictEquals(absolutizeUrl('https://example.com/aaa/bbb/ccc.odt', './doc1.svg'), 'https://example.com/aaa/bbb/doc1.svg');
+  assertStrictEquals(absolutizeUrl('https://example.com/aaa/bbb/ccc.odt', 'doc2.svg'), 'https://example.com/aaa/bbb/doc2.svg');
 });
