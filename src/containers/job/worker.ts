c8b1c2e9965c7be8e098c0ab5c63a21096f6c6a7
Grzegorz Godlewski <gg@gitgis.com>
2025-11-21 16:50:24 +0100

    Reapply "Add debug info to error message"
    This reverts commit 353919572a45f5c94f733e9b55168997e7844b53.

--- src/containers/job/worker.ts
+++ src/containers/job/worker.ts
@@ -1,4 +1,5 @@
 import {parentPort} from 'node:worker_threads';
+import fs from 'node:fs';
 import {executeOdtToMarkdown} from '../../odt/executeOdtToMarkdown.ts';
 
 parentPort.on('message', async (msg) => {
@@ -13,7 +14,11 @@ parentPort.on('message', async (msg) => {
 
     parentPort.postMessage({ err: new Error('Unhandled worker message: ' + type) });
   } catch (err) {
-    console.error(err);
+    console.error(err, msg.payload);
     parentPort.postMessage({ err });
+    if (err.message.indexOf('Corrupted zip') > -1) {
+      console.info('Corrupted zip, removing file: ', msg.payload.odtPath);
+      fs.unlinkSync(msg.payload.odtPath);
+    }
   }
 });
